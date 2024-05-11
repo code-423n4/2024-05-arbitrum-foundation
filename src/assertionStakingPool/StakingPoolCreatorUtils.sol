964d79749202bfe1d939dcd3560639eb1d403a02
sabatha7 <sabatha.bapela1@gmail.com>
2024-05-11 23:47:09 +0200

    Consider utilizing explicit access control mechanisms and validation checks to prevent unauthorized contract creations or malicious inputs.

--- src/assertionStakingPool/StakingPoolCreatorUtils.sol
+++ src/assertionStakingPool/StakingPoolCreatorUtils.sol
@@ -11,8 +11,9 @@ import "@openzeppelin/contracts/utils/Address.sol";
 library StakingPoolCreatorUtils {
     error PoolDoesntExist();
     
-    function getPool(bytes memory creationCode, bytes memory args) internal view returns (address) {
-        bytes32 bytecodeHash = keccak256(abi.encodePacked(creationCode, args));
+    function getPool(bytes memory creationCode, bytes memory args, address implementsStakingPoolCreatorUtils) internal view returns (address) {
+        require(address(this) == implementsStakingPoolCreatorUtils, "mismatch");
+		bytes32 bytecodeHash = keccak256(abi.encodePacked(creationCode, args));
         address pool = Create2.computeAddress(0, bytecodeHash, address(this));
 		if (Address.functionStaticCall(pool, new bytes(0)).length == 0) {revert PoolDoesntExist();}
         return pool;
